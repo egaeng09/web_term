de541c04642a182d911d37263b473b076b8f5093
egaeng <egaeng09@gmail.com>
2024-06-29 22:02:11 +0900

    db 수정

--- web_term.sql
+++ web_term.sql
@@ -23,20 +23,11 @@ DROP TABLE IF EXISTS `amenity`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `amenity` (
-  `id` int NOT NULL,
-  `name` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `amenity_id` int NOT NULL AUTO_INCREMENT,
+  `amenity_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`amenity_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `amenity`
---
-
-LOCK TABLES `amenity` WRITE;
-/*!40000 ALTER TABLE `amenity` DISABLE KEYS */;
-/*!40000 ALTER TABLE `amenity` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `book`
@@ -46,29 +37,23 @@ DROP TABLE IF EXISTS `book`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `book` (
-  `id` int NOT NULL,
+  `book_id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `subsite_id` int NOT NULL,
   `check_in_date` date NOT NULL,
   `check_out_date` date NOT NULL,
-  `capacity` int NOT NULL,
+  `adult` int NOT NULL,
+  `child` int NOT NULL,
   `price` int NOT NULL,
-  PRIMARY KEY (`id`),
+  `accept` tinyint NOT NULL,
+  `cancel` tinyint NOT NULL,
+  PRIMARY KEY (`book_id`),
   KEY `user_id_idx` (`user_id`),
   KEY `subsite_id_idx` (`subsite_id`),
   CONSTRAINT `subsite_id` FOREIGN KEY (`subsite_id`) REFERENCES `subsite` (`id`),
-  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `book`
---
-
-LOCK TABLES `book` WRITE;
-/*!40000 ALTER TABLE `book` DISABLE KEYS */;
-/*!40000 ALTER TABLE `book` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `campsite`
@@ -78,31 +63,22 @@ DROP TABLE IF EXISTS `campsite`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `campsite` (
-  `id` int NOT NULL,
+  `campsite_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `address` varchar(45) NOT NULL,
   `contact` varchar(45) NOT NULL,
   `information` varchar(45) DEFAULT NULL,
   `check_in_time` time NOT NULL,
   `check_out_time` time NOT NULL,
-  `thumbnail` blob,
+  `thumbnail` mediumblob,
   `start_manner_time` time DEFAULT NULL,
   `end_manner_time` time DEFAULT NULL,
   `owner_id` int NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`campsite_id`),
   KEY `owner_id_idx` (`owner_id`),
-  CONSTRAINT `owner_id` FOREIGN KEY (`owner_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `owner_id` FOREIGN KEY (`owner_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `campsite`
---
-
-LOCK TABLES `campsite` WRITE;
-/*!40000 ALTER TABLE `campsite` DISABLE KEYS */;
-/*!40000 ALTER TABLE `campsite` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `campsite_amenity`
@@ -112,25 +88,16 @@ DROP TABLE IF EXISTS `campsite_amenity`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `campsite_amenity` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `campsite_id` int NOT NULL,
   `amenity_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `amenity_id_idx` (`amenity_id`),
   KEY `campsite_id_idx` (`campsite_id`),
-  CONSTRAINT `amenity_id` FOREIGN KEY (`amenity_id`) REFERENCES `amenity` (`id`),
-  CONSTRAINT `camp_id` FOREIGN KEY (`campsite_id`) REFERENCES `campsite` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `amenity_id` FOREIGN KEY (`amenity_id`) REFERENCES `amenity` (`amenity_id`),
+  CONSTRAINT `campsite_id` FOREIGN KEY (`campsite_id`) REFERENCES `campsite` (`campsite_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `campsite_amenity`
---
-
-LOCK TABLES `campsite_amenity` WRITE;
-/*!40000 ALTER TABLE `campsite_amenity` DISABLE KEYS */;
-/*!40000 ALTER TABLE `campsite_amenity` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `review`
@@ -140,28 +107,20 @@ DROP TABLE IF EXISTS `review`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `review` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `book_id` int NOT NULL,
-  `user_id` int NOT NULL,
+  `writer_id` int NOT NULL,
   `content` varchar(45) NOT NULL,
   `star` int NOT NULL,
   `created_at` date NOT NULL,
+  `thumbnail` mediumblob,
   PRIMARY KEY (`id`),
-  KEY `book_id_idx` (`book_id`),
-  KEY `user_id_idx` (`user_id`),
-  CONSTRAINT `book_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`),
-  CONSTRAINT `writer_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `book_id_idx` (`book_id`) /*!80000 INVISIBLE */,
+  KEY `user_id_idx` (`writer_id`),
+  CONSTRAINT `book_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`),
+  CONSTRAINT `writer_id` FOREIGN KEY (`writer_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `review`
---
-
-LOCK TABLES `review` WRITE;
-/*!40000 ALTER TABLE `review` DISABLE KEYS */;
-/*!40000 ALTER TABLE `review` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `subsite`
@@ -171,26 +130,17 @@ DROP TABLE IF EXISTS `subsite`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `subsite` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `campsite_id` int NOT NULL,
-  `site_type` tinyint NOT NULL,
+  `site_type` varchar(10) NOT NULL,
   `capacity` int NOT NULL,
   `price` int NOT NULL,
-  `site_thumbnail` blob,
+  `site_thumbnail` mediumblob,
   PRIMARY KEY (`id`),
   KEY `campsite_id_idx` (`campsite_id`),
-  CONSTRAINT `campsite_id` FOREIGN KEY (`campsite_id`) REFERENCES `campsite` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `camp_id` FOREIGN KEY (`campsite_id`) REFERENCES `campsite` (`campsite_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `subsite`
---
-
-LOCK TABLES `subsite` WRITE;
-/*!40000 ALTER TABLE `subsite` DISABLE KEYS */;
-/*!40000 ALTER TABLE `subsite` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -201,26 +151,14 @@ DROP TABLE IF EXISTS `user`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `email` varchar(20) NOT NULL,
-  `id` int NOT NULL,
+  `user_id` int NOT NULL AUTO_INCREMENT,
   `password` varchar(20) NOT NULL,
   `user_type` tinyint NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping events for database 'web_term'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -231,4 +169,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-29 14:29:26
+-- Dump completed on 2024-06-29 22:01:44
